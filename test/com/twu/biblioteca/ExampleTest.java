7bd8f1d73664d290347205b8b74a1c0ff261678a
pigeonsL <pigeonsL@337450871@qq.com>
2020-04-15 19:33:20 +0800

    Test 1.1 welcome 1.4 main menu

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,40 @@
 package com.twu.biblioteca;
 
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
 
-    @Test
-    public void test() {
-        assertEquals(1, 1);
+    MainMenu mainMenu;
+    ByteArrayOutputStream output;
+    @Before
+    public void setUp(){
+        mainMenu = new MainMenu();
+        output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
     }
+    @Test //1.1 welcome
+    public void testWelcome() {
+        mainMenu.showWelcome();
+        assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!"+
+                System.getProperty("line.separator"), output.toString());
+    }
+    @Test //1.4 main menu
+    public void testOptions(){
+        mainMenu.showOptions();
+        assertEquals("Please select an option from the following:\n"+
+                        "1.List of books\r\n"+
+                        "2.Checkout a book\r\n"+
+                        "3.Return a book\r\n"+
+                        "4.Quit\r\n"
+                        , output.toString());
+    }
+
 }
